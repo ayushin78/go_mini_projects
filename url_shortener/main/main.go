019b4cf549980ef016770a459f484eef00f5b4d5
ayushin78 <ayushin78@gmail.com>
2018-12-02 14:09:50 +0530

    Uses YAMLHandler to serve at server(3000 port)
    YAMLHandler from urlshort package is used as an handler that will serve at port 3000. yaml content read from the example.yaml file and a newly allocated servemux whose implemention is yet to be completed are provided as arguments to YAMLHandler.

--- url_shortener/main/main.go
+++ url_shortener/main/main.go
@@ -3,6 +3,10 @@ package urlshort
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/ayushin78/go_mini_projects/url_shortener"
 )
 
 func main() {
@@ -11,5 +15,11 @@ func main() {
 		fmt.Println("Error : File could not be read")
 		return
 	}
+
+	mux := http.NewServeMux()
+	yamlhandler, err := urlshort.YAMLHandler(content, mux)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", yamlhandler)
 
 }
