4d26dccba3854c07782a63b9d20ea795c84e602f
ayushin78 <ayushin78@gmail.com>
2018-12-09 20:27:04 +0530

    Add all files

---
+++ choose_adventure/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func main() {
+
+	adventure, err := parseJSON()
+	if err != nil {
+		panic(err)
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/cyoa/", http.HandlerFunc(showIntro))
+	ah := adventureHandler(adventure, mux)
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", ah)
+
+}
+
+func parseJSON() (map[string]interface{}, error) {
+	var adventure map[string]interface{}
+	var filepath = flag.String("path", "./story.json", "path of the JSON file")
+	flag.Parse()
+	content, err := ioutil.ReadFile(*filepath)
+	if err != nil {
+		fmt.Println("Error : File could not be read")
+		return nil, err
+	}
+
+	err = json.Unmarshal(content, &adventure)
+	if err != nil {
+		return nil, err
+	}
+
+	return adventure, nil
+}
+
+func adventureHandler(adventure map[string]interface{}, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("templates/story.html")
+		path := strings.TrimLeft(r.URL.Path, "/cyoa/")
+		arc, ok := adventure[path]
+		if ok {
+			err := t.Execute(w, arc)
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
+}
+
+func showIntro(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/cyoa/intro", http.StatusFound)
+}
